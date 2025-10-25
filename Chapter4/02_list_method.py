fb71951a2a04e5a6e5c2628f30f11c6887c761a3
sourenroy <168115446+sourenroy@users.noreply.github.com>
2025-10-25 10:31:53 +0530

    Chapter 4
    list, list methods, tuple, count, index, length

---
+++ Chapter4/02_list_method.py
@@ -0,0 +1,17 @@
+fruits = ["apple", "mango", "guava", 5, 354.35, False, True]
+
+fruits.append("Souren")
+print(fruits)
+
+l1 = [1,20, 8, 5, 88, 7, 25]
+l1.sort()
+print(l1)
+
+l1.reverse()
+print(l1)
+
+l1.insert(3, 9090)
+print(l1)
+
+l1.pop(2)
+print(l1)
