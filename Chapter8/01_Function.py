7d03102df792c6e4908e914e4a79fdfae24f716a
sourenroy <168115446+sourenroy@users.noreply.github.com>
2025-10-30 14:57:38 +0530

    chapter 8

---
+++ Chapter8/01_Function.py
@@ -0,0 +1,9 @@
+def avg():
+    num1 = int(input("Enter the number: "))
+    num2 = int(input("Enter the number: "))
+    num3 = int(input("Enter the number: "))
+
+    ave = (num1 + num2 + num3)/3
+    print("Average",ave)
+
+avg()
